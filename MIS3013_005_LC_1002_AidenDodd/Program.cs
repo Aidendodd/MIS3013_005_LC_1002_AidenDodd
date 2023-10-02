f2254570eb91669bc14c72f9c7d2dbfcdd0c9498
dodd0027 <dodd0027@L97BYTV3>
2023-10-02 14:08:30 -0500

    Add project files.

---
+++ MIS3013_005_LC_1002_AidenDodd/Program.cs
@@ -0,0 +1,21 @@
+﻿//MIS 3013 005
+//October 02, 2023
+//Aiden Dodd, 113556164
+Console.WriteLine("Functions");
+
+int age;
+age=Convert.ToInt32("23");// function call
+
+double r;
+
+r=adddouble(10,20.6);
+
+Console.WriteLine(r);
+
+//add of two double values, the result is a double 
+double adddouble(double x1, double x2)
+{
+    double r;
+    r = x1 + x2;
+    return r;
+}
